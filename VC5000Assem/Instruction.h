3a17f54d84b70f9de600918d08e0bb01c36ea975
Aayam Shrestha <aayam2071@gmail.com>
2021-05-12 16:05:37 -0500

    Added function to parse line into label, op code, operand1, operand2 in Instruction.h

--- VC5000Assem/Instruction.h
+++ VC5000Assem/Instruction.h
@@ -2,6 +2,7 @@
 // Class to parse and provide information about instructions.  Note: you will be adding more functionality.
 //
 #pragma once
+#include<sstream>
 
 // The elements of an instruction.
 class Instruction{
@@ -24,11 +25,17 @@ public:
         //removing comments from a line
         removeComments(a_line);
 
-        
+        if (ParseLineIntoFields)
+        {
+            //CHECKCHECK report error - failed to parse b/c instruction was invalid !!!!!!!!
+        }
+
+
 
         return ST_MachineLanguage; //CHECKCHECK THIS IS TEMPORARY!!!
     }
 
+    //using code from the website to remove comments and parse instruction into label, op code, operand1, operand2
     void removeComments(string a_line)
     {
         // Demonstrating how to remove a semicolon and everything after it from a string
@@ -43,7 +50,34 @@ public:
         return;
     }
 
+    // Function to break down a statement into label, op code, operand1, operand2
+    // If it returns an empty string for the op code, it indicates an empty string
+    // The return value is false if there are extra fields.
+    // Make sure you include the algorithm file.
+    bool ParseLineIntoFields(string a_line, string& a_label, string& a_OpCode,
+        string& a_Operand1, string& a_Operand2)
+    {
+        // Get rid of any commas from the line.
+        a_line.replace(a_line.begin(), a_line.end(), ',', ' ');
 
+        // Get the elements of the line.  That is the label, op code, operand1, and operand2.
+        string endStr;
+        a_label = a_OpCode = a_Operand1 = a_Operand2 = "";
+        istringstream ins(a_line);
+        if (a_line[0] == ' ' || a_line[0] == '\t')
+        {
+            a_label = "";
+            ins >> a_OpCode >> a_Operand1 >> a_Operand2 >> endStr;
+        }
+        else
+        {
+            ins >> a_label >> a_OpCode >> a_Operand1 >> a_Operand2 >> endStr;
+        }
+        // If there is extra data, return false.
+        return endStr.empty() ? true : false;
+    }
+
+    
 
     // Compute the location of the next instruction.
     int LocationNextInstruction(int a_loc) { return a_loc + 1; }; //CHECKCHECK
